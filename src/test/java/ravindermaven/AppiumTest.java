a854b9c192b97b0f942421b1d312bcbd2c939e8e
ravinderk902 <ravinderk902@gmail.com>
2020-07-03 11:21:11 +0000

    first commit

---
+++ src/test/java/ravindermaven/AppiumTest.java
@@ -0,0 +1,13 @@
+package ravindermaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AppiumTestCaseTest() {
+
+		System.out.println("AppiumTestCaseChrome");
+	}
+
+}
---
+++ src/test/java/ravindermaven/SeleniumTest.java
@@ -0,0 +1,19 @@
+package ravindermaven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void SeleniumTestCaseChrome() {
+
+		System.out.println("SeleniumTestCaseChrome");
+	}
+
+	@Test
+	public void SeleniumTestCaseFirefox() {
+
+		System.out.println("SeleniumTestCaseFirefox");
+	}
+
+}
